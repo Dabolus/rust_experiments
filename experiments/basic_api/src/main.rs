325cfeae6ba6aeda377e6c2fbc342535de3fc831
Giorgio Garasto <giorgio@garasto.it>
2019-12-12 22:52:43 +0100

    feat: implement basic API

---
+++ experiments/basic_api/src/main.rs
@@ -0,0 +1,45 @@
+use actix_web::middleware::Logger;
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/{id}/{name}")]
+async fn parametrized(info: web::Path<(u32, String)>) -> impl Responder {
+    format!("Hello, {}! id: {}", info.1, info.0)
+}
+
+#[get("/*")]
+async fn index() -> impl Responder {
+    format!("Hello everything else!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    let port: u16 = std::env::var("PORT")
+        .ok()
+        .and_then(|p| p.parse().ok())
+        .unwrap_or(3000);
+
+    // Start http server
+    let server = HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .service(parametrized)
+            .service(index)
+    })
+    .bind(format!("0.0.0.0:{}", port))?
+    .start();
+
+    println!("Server listening on port {}", port);
+
+    server.await
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
